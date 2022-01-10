9f29c0c3581678c04b94c257e6534b148c4f3630
asdiann <54527711+asdiann@users.noreply.github.com>
2022-01-10 13:25:12 +0700

    update main.py
    change https://techwithtim.net to https://www.cermati.com/

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 PATH = "C:\chromedriver_win32\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://techwithtim.net")
+driver.get("https://www.cermati.com/")
 print(driver.title)
 
 
